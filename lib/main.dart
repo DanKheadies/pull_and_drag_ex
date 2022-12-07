2fc1175d846ccb7175a2c11b6dc3aafffc2f488c
Papa Daco <papadaco@PapaDaco.local>
2022-12-06 19:27:02 -0500

    Draggable colors on tap; pre-shifting

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 
 void main() {
@@ -30,11 +32,32 @@ class MyHomePage extends StatefulWidget {
 
 class _MyHomePageState extends State<MyHomePage> {
   double touchOpac = 1;
+  String selectedColor = '';
+  bool canDrag = true;
+
+  void colorSelected(String color) {
+    print('selected: $color');
+    setState(() {
+      selectedColor = color;
+    });
+    // Timer(const Duration(seconds: 2), () {
+    //   setState(() {
+    //     canDrag = false;
+    //   });
+    // });
+  }
+
+  void resetDraggables() {
+    print('resetting');
+    setState(() {
+      selectedColor = '';
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     final double squareSize = MediaQuery.of(context).size.width / 3.75;
-    print(squareSize);
+    // print(squareSize);
 
     return Scaffold(
       backgroundColor: Colors.black,
@@ -49,8 +72,8 @@ class _MyHomePageState extends State<MyHomePage> {
                 height: double.infinity,
                 decoration: const BoxDecoration(
                   borderRadius: BorderRadius.only(
-                    topLeft: Radius.circular(25),
-                    topRight: Radius.circular(25),
+                    topLeft: Radius.circular(40),
+                    topRight: Radius.circular(40),
                   ),
                   color: Colors.grey,
                 ),
@@ -63,8 +86,8 @@ class _MyHomePageState extends State<MyHomePage> {
                 height: double.infinity,
                 decoration: const BoxDecoration(
                   borderRadius: BorderRadius.only(
-                    bottomLeft: Radius.circular(25),
-                    bottomRight: Radius.circular(25),
+                    bottomLeft: Radius.circular(40),
+                    bottomRight: Radius.circular(40),
                   ),
                   color: Colors.white,
                 ),
@@ -100,6 +123,11 @@ class _MyHomePageState extends State<MyHomePage> {
                         size: squareSize,
                         dataColor: 'pink',
                         color: Colors.pink,
+                        canAccept:
+                            selectedColor != 'pink' && selectedColor != '',
+                        canDrag: canDrag,
+                        onTapDown: colorSelected,
+                        onTapUp: resetDraggables,
                       ),
                       DragObject(
                         left: 110,
@@ -107,6 +135,11 @@ class _MyHomePageState extends State<MyHomePage> {
                         size: squareSize,
                         dataColor: 'red',
                         color: Colors.red,
+                        canAccept:
+                            selectedColor != 'red' && selectedColor != '',
+                        canDrag: canDrag,
+                        onTapDown: colorSelected,
+                        onTapUp: resetDraggables,
                       ),
                       DragObject(
                         left: 220,
@@ -114,6 +147,11 @@ class _MyHomePageState extends State<MyHomePage> {
                         size: squareSize,
                         dataColor: 'deepOrange',
                         color: Colors.deepOrange,
+                        canAccept: selectedColor != 'deepOrange' &&
+                            selectedColor != '',
+                        canDrag: canDrag,
+                        onTapDown: colorSelected,
+                        onTapUp: resetDraggables,
                       ),
                       DragObject(
                         left: 0,
@@ -121,6 +159,11 @@ class _MyHomePageState extends State<MyHomePage> {
                         size: squareSize,
                         dataColor: 'orange',
                         color: Colors.orange,
+                        canAccept:
+                            selectedColor != 'orange' && selectedColor != '',
+                        canDrag: canDrag,
+                        onTapDown: colorSelected,
+                        onTapUp: resetDraggables,
                       ),
                       DragObject(
                         left: 110,
@@ -128,6 +171,11 @@ class _MyHomePageState extends State<MyHomePage> {
                         size: squareSize,
                         dataColor: 'yellow',
                         color: Colors.yellow,
+                        canAccept:
+                            selectedColor != 'yellow' && selectedColor != '',
+                        canDrag: canDrag,
+                        onTapDown: colorSelected,
+                        onTapUp: resetDraggables,
                       ),
                       DragObject(
                         left: 220,
@@ -135,6 +183,11 @@ class _MyHomePageState extends State<MyHomePage> {
                         size: squareSize,
                         dataColor: 'green',
                         color: Colors.green,
+                        canAccept:
+                            selectedColor != 'green' && selectedColor != '',
+                        canDrag: canDrag,
+                        onTapDown: colorSelected,
+                        onTapUp: resetDraggables,
                       ),
                       DragObject(
                         left: 0,
@@ -142,6 +195,11 @@ class _MyHomePageState extends State<MyHomePage> {
                         size: squareSize,
                         dataColor: 'teal',
                         color: Colors.teal,
+                        canAccept:
+                            selectedColor != 'teal' && selectedColor != '',
+                        canDrag: canDrag,
+                        onTapDown: colorSelected,
+                        onTapUp: resetDraggables,
                       ),
                       DragObject(
                         left: 110,
@@ -149,6 +207,11 @@ class _MyHomePageState extends State<MyHomePage> {
                         size: squareSize,
                         dataColor: 'cyan',
                         color: Colors.cyan,
+                        canAccept:
+                            selectedColor != 'cyan' && selectedColor != '',
+                        canDrag: canDrag,
+                        onTapDown: colorSelected,
+                        onTapUp: resetDraggables,
                       ),
                       DragObject(
                         left: 220,
@@ -156,6 +219,11 @@ class _MyHomePageState extends State<MyHomePage> {
                         size: squareSize,
                         dataColor: 'blue',
                         color: Colors.blue,
+                        canAccept:
+                            selectedColor != 'blue' && selectedColor != '',
+                        canDrag: canDrag,
+                        onTapDown: colorSelected,
+                        onTapUp: resetDraggables,
                       ),
                       DragObject(
                         left: 00,
@@ -163,6 +231,11 @@ class _MyHomePageState extends State<MyHomePage> {
                         size: squareSize,
                         dataColor: 'indigo',
                         color: Colors.indigo,
+                        canAccept:
+                            selectedColor != 'indigo' && selectedColor != '',
+                        canDrag: canDrag,
+                        onTapDown: colorSelected,
+                        onTapUp: resetDraggables,
                       ),
                       DragObject(
                         left: 110,
@@ -170,6 +243,11 @@ class _MyHomePageState extends State<MyHomePage> {
                         size: squareSize,
                         dataColor: 'purple',
                         color: Colors.purple,
+                        canAccept:
+                            selectedColor != 'purple' && selectedColor != '',
+                        canDrag: canDrag,
+                        onTapDown: colorSelected,
+                        onTapUp: resetDraggables,
                       ),
                       DragObject(
                         left: 220,
@@ -177,7 +255,35 @@ class _MyHomePageState extends State<MyHomePage> {
                         size: squareSize,
                         dataColor: 'deepPurple',
                         color: Colors.deepPurple,
+                        canAccept: selectedColor != 'deepPurple' &&
+                            selectedColor != '',
+                        canDrag: canDrag,
+                        onTapDown: colorSelected,
+                        onTapUp: resetDraggables,
                       ),
+                      // Positioned(
+                      //   left: 110,
+                      //   top: 330,
+                      //   child: DragTarget(
+                      //     builder: ((context, candidateData, rejectedData) {
+                      //       return Container(
+                      //         height: squareSize,
+                      //         width: squareSize,
+                      //         color: Colors.purple,
+                      //       );
+                      //     }),
+                      //     onWillAccept: (data) {
+                      //       print('purple will accept $data');
+                      //       return data == 'purple';
+                      //     },
+                      //     onAccept: (data) {
+                      //       print('purple accepted $data');
+                      //     },
+                      //     onLeave: (data) {
+                      //       print('$data left purple');
+                      //     },
+                      //   ),
+                      // ),
                     ],
                   ),
                 ),
@@ -245,6 +351,10 @@ class DragObject extends StatelessWidget {
     required this.size,
     required this.dataColor,
     required this.color,
+    required this.canAccept,
+    required this.canDrag,
+    required this.onTapDown,
+    required this.onTapUp,
   }) : super(key: key);
 
   final double left;
@@ -252,52 +362,70 @@ class DragObject extends StatelessWidget {
   final double size;
   final String dataColor;
   final Color color;
+  final bool canAccept;
+  final bool canDrag;
+  final Function(String) onTapDown;
+  final VoidCallback onTapUp;
 
   @override
   Widget build(BuildContext context) {
+    // print('$dataColor can accept: $canAccept');
     return Positioned(
       left: left,
       top: top,
-      child: Stack(
-        children: [
-          DragTarget(
-            builder: ((context, candidateData, rejectedData) {
-              return Container(
-                height: size,
-                width: size,
-                color: Colors.transparent,
-              );
-            }),
-            onWillAccept: (data) {
-              print('will accept: $data');
-              return data == dataColor;
-            },
-            onAccept: (data) {
-              print('accepted: $data');
-            },
-            onLeave: (data) {
-              print('on leave: $data');
-            },
-          ),
-          Draggable<String>(
-            data: dataColor,
-            feedback: Container(
-              height: size,
-              width: size,
-              color: color.withOpacity(0.666),
-            ),
-            childWhenDragging: Container(
-              height: size,
-              width: size,
-              color: color.withOpacity(0.333),
-            ),
-            child: Container(
-              height: size,
-              width: size,
-              color: color,
-            ),
-          ),
-        ],
+      child: InkWell(
+        onTapDown: (_) => onTapDown(dataColor),
+        // onTapCancel: () => onTapUp,
+        child: Stack(
+          children: [
+            canAccept
+                ? DragTarget(
+                    builder: ((context, candidateData, rejectedData) {
+                      return Container(
+                        height: size,
+                        width: size,
+                        // color: Colors.transparent,
+                        color: color,
+                      );
+                    }),
+                    onWillAccept: (data) {
+                      print('$dataColor will accept $data');
+                      return data == dataColor;
+                    },
+                    onAccept: (data) {
+                      print('$dataColor accepted $data');
+                    },
+                    onLeave: (data) {
+                      print('$data left $dataColor');
+                    },
+                  )
+                : Draggable<String>(
+                    data: dataColor,
+                    feedback: Container(
+                      height: size,
+                      width: size,
+                      color: color.withOpacity(0.666),
+                    ),
+                    childWhenDragging: Container(
+                      height: size,
+                      width: size,
+                      color: color.withOpacity(0.333),
+                    ),
+                    onDragEnd: (_) => onTapUp(),
+                    child: Container(
+                      height: size,
+                      width: size,
+                      color: color,
+                    ),
+                  ),
+
+            // Container(
+            //   width: size,
+            //   height: size,
+            //   color: Colors.black,
+            // ),
+          ],
+        ),
       ),
     );
   }
